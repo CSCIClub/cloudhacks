ecf8f978dc74187388c8d9169fe26e1588dbff6e
Suyash Bhatt <suyash1629@gmail.com>
2017-02-04 05:07:00 -0600

    use https instead of http

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://sendmail.csci.club/contact_me.php",
+                url: "https://sendmail.csci.club/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
